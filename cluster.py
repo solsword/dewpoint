4b2093a31c0014c81e118af7549251146a8d1fa8
Peter Mawhorter <pmawhorter@gmail.com>
2017-07-07 13:07:43 -0400

    Added simplex_grid github submodule in order to help visualize extracted manifolds (it doesn't help that much; grid construction is hard). Added manifold analysis code to cluster.py.
    -Peter Mawhorter

--- cluster.py
+++ cluster.py
@@ -16,9 +16,11 @@ import matplotlib.pyplot as plt
 from matplotlib import collections as mc
 from sklearn.metrics import pairwise
 from sklearn.neighbors import NearestNeighbors
-from sklearn.utils.graph import graph_shortest_path
+from sklearn.utils.graph import graph_shortest_path as graph_shortest_paths
 from scipy.stats import linregress
 from scipy.spatial.distance import euclidean
+
+from simplex_grid.simplex_grid import simplex_grid
 
 COLORS = [
   (0.0, 0.2, 0.8),
@@ -54,8 +56,10 @@ NORMALIZE_DISTANCES = False
 NORMALIZATION_NEIGHBORHOOD = 4
 NORMALIZATION_STRENGTH = 0.25
 
-MANIFOLD_ERROR_THRESHOLD = 0.5
-MANIFOLD_APPROXIMATION_SAMPLES = 4
+MANIFOLD_ERROR_THRESHOLD = 0.02
+MANIFOLD_APPROXIMATION_SAMPLES = 5
+MANIFOLD_GRID_RESOLUTION = 10
+MANIFOLD_EDGE_SAMPLES = 100
 
 PLOT = [
   #"averages",
@@ -65,7 +69,7 @@ PLOT = [
   #"neighbor_counts",
   #"impact",
   #"impact_ratio",
-  "significance",
+  #"significance",
   #"local_structure",
   #"normalized_distances",
   #"coherence",
@@ -78,7 +82,7 @@ PLOT = [
   #"raw",
   #"absolute_growth",
   "results",
-  "result_stats",
+  #"result_stats",
 ]
 
 # TODO: Testing
@@ -844,6 +848,7 @@ def cluster(points, metric="euclidean"):
     if sqw * sqh == 1:
       fix, ax = plt.subplots()
       plt.title("Singular Clustering")
+      plt.axis("equal")
       plt.scatter(projected[:,0], projected[:,1], s=0.8, c=POINT_COLOR)
       data = []
       colors = []
@@ -862,6 +867,7 @@ def cluster(points, metric="euclidean"):
       ax.margins(0.1)
     else:
       fig, axes = plt.subplots(sqh, sqw, sharex=True, sharey=True)
+      plt.axis("equal")
       fig.suptitle("{} Clusterings".format(len(clusterings)))
       for i, clstr in enumerate(clusterings):
         sqx, sqy = i % sqw, i // sqw
@@ -980,7 +986,7 @@ def cluster(points, metric="euclidean"):
     plt.legend()
     plt.xlabel("Cluster Stats")
 
-  plt.show()
+  #plt.show()
   print("...done with clustering.")
 
   best = sorted(clusterings, key=lambda x: x[2])[-1]
@@ -988,7 +994,7 @@ def cluster(points, metric="euclidean"):
   raw_asg = list(set([unions.find(i) for i in range(len(points))]))
   assignments = [raw_asg.index(unions.find(i)) for i in range(len(points))]
 
-  return points, (nbd, nbi), clusterings, assignments
+  return points, (nbd, nbi), best, assignments, clusterings
 
 def subclusters(points, neighbors, clustering):
   nbd, nbi = neighbors
@@ -1015,6 +1021,9 @@ def subclusters(points, neighbors, clustering):
     for fr, to, d, nr in edges:
       if unions.find(fr) == anchor:
         sc["core_edges"].append((fr, to, d))
+        # ensure connectivity of all_edges graph:
+        if (fr, to, d) not in sc["all_edges"]:
+          sc["all_edges"].append((fr, to, d))
 
     results.append(sc)
 
@@ -1025,113 +1034,322 @@ def analyze_clustering(points, neighbors, clustering):
   if len(subcl) == 0:
     print("No interesting subclusters to analyze.")
   else:
-    cluster = subcl[0]
-    manifold = approximate_manifold(points, cluster)
-    mdim = 1 + (len(manifold["anchors"]) - 2) / 2
-    print("Cluster size: {}".format(len(cluster["points"])))
-    print("Manifold edges: {}".format(len(cluster["all_edges)"])))
-    print("Manifold dimensions: {}".format(mdim))
-    plt.figure()
-    plt.title("Mapped Cluster ({}-dimensional)".format(mdim))
+    for cluster in subcl:
+      manifold = approximate_manifold(points, cluster)
+      mdim = 1 + (len(manifold["anchors"]) - 2) / 2
+      print("Cluster size: {}".format(len(cluster["points"])))
+      print("Manifold edges: {}".format(len(cluster["all_edges"])))
+      print("Manifold dimensions: {}".format(mdim))
+      plot_manifold(manifold, show_interior=False, show_edges=True)
+    #plt.show()
+
+def plot_manifold(
+  manifold,
+  show_interior=False,
+  show_grid=False,
+  show_edges=False
+):
+    n = manifold["exterior"].shape[0]
+    nanchors = len(manifold["anchors"])
+    mdim = 1 + (nanchors - 2) / 2
+    fig, ax = plt.subplots()
+    plt.title("Detected Manifold ({}-dimensional)".format(mdim))
+    plt.axis("equal")
+
+    # Manifold edges:
+    earray = []
+    for fr, to, d in manifold["edges"]:
+      earray.append((manifold["exterior"][fr], manifold["exterior"][to]))
+    earray = np.asarray(earray)
+    lc = mc.LineCollection(
+      earray,
+      colors=(0.8, 0.8, 0.8),
+      linewidths=0.7
+    )
+    ax.add_collection(lc)
+
+    # Original points:
     plt.scatter(
       manifold["exterior"][:,0],
       manifold["exterior"][:,1],
-      s=0.8,
-      c=DESAT[0]
+      s=1.0,
+      c=[(0.9, 0.9, 0.9)]*n
     )
-    if mdim == 1:
-      plt.scatter(
-        manifold["interior"][:,0],
-        np.mean(manifold["exterior"][:1]),
-        s=0.8,
-        c=COLORS[0]
-      )
-    else:
-      plt.scatter(
-        manifold["interior"][:,0],
-        manifold["interior"][:,1],
-        s=0.8,
-        c=COLORS[0]
-      )
-    plt.show()
 
+    # Anchor points:
+    apoints = manifold["exterior"][manifold["anchors"],:]
+    plt.scatter(
+      apoints[:,0],
+      apoints[:,1],
+      s=5.0,
+      c=[(1.0, 0.0, 0.0)]*apoints.shape[0]
+    )
+
+    # Reconstructed points:
+    plt.scatter(
+      manifold["reconstructed"][:,0],
+      manifold["reconstructed"][:,1],
+      s=0.8,
+      c=[(0.0, 0.4, 0.05)]*n
+    )
+
+    # Interior Points (at most 2 anchors-worth):
+    if show_interior:
+      if mdim < 1:
+        plt.scatter(
+          manifold["interior"][:,0],
+          np.full(
+            (manifold["interior"].shape[0],),
+            np.mean(manifold["exterior"][:1])
+          ),
+          s=0.8,
+          c=[(0.0, 0.1, 0.6)]*n
+        )
+      else:
+        plt.scatter(
+          manifold["interior"][:,0],
+          manifold["interior"][:,1],
+          s=0.8,
+          c=[(0.4, 0.6, 1.0)]*n
+        )
+
+    # Grid:
+    if show_grid:
+      print("Building manifold grid...")
+      # A uniform grid over a unit simplex:
+      gridpoints = simplex_grid(
+        nanchors,
+        MANIFOLD_GRID_RESOLUTION
+      ) / MANIFOLD_GRID_RESOLUTION
+      neighbors = NearestNeighbors(
+        n_neighbors=nanchors,
+        algorithm="ball_tree"
+      ).fit(gridpoints)
+      nbd, nbi = neighbors.kneighbors(gridpoints)
+      gridedges = []
+      for i in range(len(nbi)):
+        for nb in nbi[i]:
+          gridedges.append((i, nb))
+      print("  ...done building grid.")
+
+      print(
+        "Translating grid into original space ({} points)...".format(
+          len(gridpoints)
+        )
+      )
+      extpoints = []
+      for i, gp in enumerate(gridpoints):
+        prbar(i / len(gridpoints))
+        extpoints.append(approximate_exterior_position(manifold, gp))
+      extpoints = np.asarray(extpoints)
+
+      extedges = [ (extpoints[i], extpoints[j]) for (i, j) in gridedges ]
+      print("  ...done translating grid.")
+
+      plt.scatter(
+        extpoints[:,0],
+        extpoints[:,1],
+        s=0.4,
+        c=[(0.0, 0.0, 0.0)]*n
+      )
+      lc = mc.LineCollection(
+        extedges,
+        colors=(0.2, 0.2, 0.2),
+        linewidths=0.7
+      )
+      ax.add_collection(lc)
+
+    # Edges:
+    if show_edges:
+      print("Building manifold edges...")
+      extrema = np.asarray([
+        [0]*i + [1] + [0]*(nanchors - i - 1)
+          for i in range(nanchors)
+      ])
+      edges = []
+      for i, fr in enumerate(extrema):
+        for j, to in enumerate(extrema[i:]):
+          edges.append(np.asarray([
+            fr*(interp/MANIFOLD_EDGE_SAMPLES)
+          + to * (MANIFOLD_EDGE_SAMPLES - interp)/MANIFOLD_EDGE_SAMPLES
+              for interp in range(MANIFOLD_EDGE_SAMPLES+1)
+          ]))
+      print("  ...done building edges.")
+
+
+      print("Translating edges into original space...")
+      exedges = []
+      for e in edges:
+        ex = []
+        for p in e:
+          ex.append(approximate_exterior_position(manifold, p))
+        exedges.append(ex)
+      exedges = np.asarray(exedges)
+
+      elen = exedges.shape[1]
+
+      exlines = []
+      for e in exedges:
+        el = []
+        for i in range(len(e)-1):
+          el.append((e[i], e[i+1]))
+        exlines.append(el)
+      exlines = np.asarray(exlines)
+      print("  ...done translating edges.")
+
+      for i in range(len(exedges)):
+        plt.scatter(
+          exedges[i][:,0],
+          exedges[i][:,1],
+          s=0.4,
+          c=[(0.0, 0.0, 0.0)]*elen
+        )
+        lc = mc.LineCollection(
+          exlines[i],
+          colors=[(0.2, 0.2, 0.2)]*(elen-1),
+          linewidths=0.7
+        )
+        ax.add_collection(lc)
+
+def floyd_warshall(earray):
+  # TODO: Get rid of this?
+  n = earray.shape[0]
+  for shorter in range(n):
+    for fr in range(n):
+      #if fr == shorter:
+      #  continue
+      for to in range(n):
+        #if fr == to:
+        #  continue
+        better = earray[fr,shorter] + earray[shorter, to]
+        if better < earray[fr, to]:
+          earray[fr, to] = better
+
+def collapse_cluster(points, cluster):
+  result = {
+    "count": 0,
+    "scale": 0,
+    "points": [],
+    "edges": [],
+  }
+  pidmap = {}
+
+  for pid in cluster["points"]:
+    pidmap[pid] = len(result["points"])
+    result["points"].append(points[pid])
+
+  result["points"] = np.asarray(result["points"])
+  result["count"] = result["points"].shape[0]
+
+  result["scale"] = 0
+  result["edges"] = []
+  for fr, to, d in cluster["all_edges"]:
+    result["scale"] += d
+    result["edges"].append((pidmap[fr], pidmap[to], d))
+
+  result["scale"] /= len(result["edges"])
+
+  return result
+
+# TODO: Relax points beforehand? Deal with noise better somehow!
 def approximate_manifold(points, cluster):
   # Find the shortest-path-distances-matrix for our cluster:
-  cluster_points = []
-  pidmap = {}
-  for pid in cluster["points"]:
-    pidmap[pid] = len(cluster_points)
-    cluster_points.append(points[pid])
+  local = collapse_cluster(points, cluster)
 
-  cluster_points = np.asarray(cluster_points)
-  n = cluster_points.shape[0]
-
-  mean_edge_length = 0
-  cluster_edges = []
-  for fr, to, d in cluster["all_edges"]:
-    mean_edge_length += d
-    cluster_edges.append((pidmap[fr], pidmap[to], d))
-
-  mean_edge_length /= len(cluster_edges)
-
-  longest_edge = max(d for fr, to, d in cluster_edges)
-  edgematrix = np.full((n, n), longest_edge*n*n)
-  for fr, to, d in cluster_edges:
+  longest_edge = max(d for fr, to, d in local["edges"])
+  nonedge = longest_edge*local["count"]**2
+  edgematrix = np.full((local["count"], local["count"]), nonedge)
+  for fr, to, d in local["edges"]:
     edgematrix[fr,to] = d
+    edgematrix[to,fr] = d
 
-  shortest_paths = graph_shortest_path(edgematrix)
+  for i in range(local["count"]):
+    edgematrix[i,i] = 0
+
+  shortest_paths = graph_shortest_paths(edgematrix)
+  if np.max(shortest_paths) == nonedge:
+    raise RuntimeError("Cluster edges graph is not fully connected!")
 
   start = 0 # pick an arbitrary point
   extrema = [np.argmax(shortest_paths[start,:])]
   extrema.append(np.argmax(shortest_paths[extrema[0],:]))
   manifold_approximation = {
-    "count": n,
-    "scale": mean_edge_length,
+    "count": local["count"],
+    "scale": local["scale"],
     "anchors": extrema,
-    "exterior": cluster_points,
+    "exterior": local["points"],
     "interior": [],
+    "reconstructed": [],
+    "edges": local["edges"],
     "paths": shortest_paths,
   }
   compute_interior_positions(manifold_approximation)
-  rerr = reconstruction_error(manifold_approximation)
+  rerr = compute_reconstruction(manifold_approximation)
+  prerr = rerr
+  improvement = 1.0
   icount = 0
-  while rerr > MANIFOLD_ERROR_THRESHOLD:
+  print(
+    "Baseline reconstruction error ({} anchors): {:.5f}".format(
+      len(manifold_approximation["anchors"]),
+      rerr
+    )
+  )
+  out_of_extrema = False
+  while improvement > MANIFOLD_ERROR_THRESHOLD:
     icount += 1
+    distances = manifold_approximation["paths"][
+      manifold_approximation["anchors"],
+      :
+    ]
+    combined = np.sum(distances**0.5, axis=0)**2
+    bidx = np.argmax(combined)
+
+    # TODO: Get rid of this debug code:
+    #plot_manifold(manifold_approximation)
+    #plt.show()
+    if bidx in manifold_approximation["anchors"]:
+      print(
+        "Warning: failed to find new extremum (found {} given {}).".format(
+          bidx,
+          manifold_approximation["anchors"],
+        )
+      )
+      out_of_extrema = True
+      break
+    manifold_approximation["anchors"].append(bidx)
+    compute_interior_positions(manifold_approximation)
+    rerr = compute_reconstruction(manifold_approximation)
+    improvement = prerr - rerr
+    prerr = rerr
     print(
-      "[{}] Median reconstruction error ({} anchors): {}".format(
+      "[{}] Median reconstruction error ({} anchors): {:.5f} {:.3f}".format(
         icount,
         len(manifold_approximation["anchors"]),
-        rerr
+        rerr,
+        -improvement
       )
     )
-    distances = []
-    for anchor in manifold_approximation["anchors"]:
-      distances.append(manifold_approximation["paths"][anchor,:])
-    bidx = -1
-    bsum = None
-    for i in range(len(distances[0])):
-      dsum = sum(d[i] for d in distances)
-      if bsum == None or dsum > bsum:
-        bsum = dsum
-        bidx = i
-    manifold_approximation["anchors"].append(bidx)
-    rerr = reconstruction_error(manifold_approximation)
+
+  # Last anchor didn't improve things much, so remove it (not the case if we
+  # ran out of extrema).
+  if not out_of_extrema:
+    manifold_approximation["anchors"] = manifold_approximation["anchors"][:-1]
+    compute_interior_positions(manifold_approximation)
+    compute_reconstruction(manifold_approximation)
 
   return manifold_approximation
 
 def find_manifold_position(manifold, manifold_point_index):
-  alines = np.asarray([
-    manifold["paths"][manifold_point_index, anchor]
-      for anchor in manifold["anchors"]
-  ])
+  alines = manifold["paths"][manifold_point_index, manifold["anchors"]]
   return alines / np.sum(alines)
 
 def compute_interior_positions(manifold):
   manifold["interior"] = []
   for mpi in range(manifold["count"]):
     manifold["interior"].append(find_manifold_position(manifold, mpi))
+  manifold["interior"] = np.asarray(manifold["interior"])
 
-def approximate_full_position(manifold, manifold_position):
+def approximate_exterior_position(manifold, manifold_position):
   best = []
   for i in range(manifold["count"]):
     best.append(
@@ -1140,9 +1358,11 @@ def approximate_full_position(manifold, manifold_position):
     best = sorted(best, key=lambda x: x[1])[:MANIFOLD_APPROXIMATION_SAMPLES]
 
   # invert & normalize distances:
-  max_best = sum(bd for (bi, bd) in best)
+  max_best = max(bd for (bi, bd) in best)
+  second_best = min(bd for (bi, bd) in best if bd != 0)
+  # TODO: Better way to avoid the singularity here?
   for i in range(len(best)):
-      best[i][1] = (1 + max_best) / (1 + best[i][1])
+      best[i][1] = (max_best + second_best) / (best[i][1] + second_best)
   sum_inv = sum(bs for (bi, bs) in best)
   for i in range(len(best)):
       best[i][1] /= sum_inv
@@ -1152,11 +1372,15 @@ def approximate_full_position(manifold, manifold_position):
     approx += manifold["exterior"][bi] * nbs
   return approx
 
-def reconstruction_error(manifold):
+def compute_reconstruction(manifold):
   errors = []
+  manifold["reconstructed"] = []
   for i, ip in enumerate(manifold["interior"]):
-    rp = approximate_full_position(manifold, ip)
+    rp = approximate_exterior_position(manifold, ip)
+    manifold["reconstructed"].append(rp)
     errors.append(euclidean(rp, manifold["exterior"][i]) / manifold["scale"])
+
+  manifold["reconstructed"] = np.asarray(manifold["reconstructed"])
 
   return np.median(errors)
 
@@ -1470,14 +1694,12 @@ IRIS_LABELS = [
 def test():
   #for tc in test_cases[4:6]:
   #for tc in test_cases[3:4]:
-  for tc in test_cases[4:9]:
+  #for tc in test_cases[4:9]:
   #for tc in test_cases[6:]:
-  #for tc in test_cases:
-    points, neighbors, clusterings, assignments = cluster(tc)
-    if len(clusterings) > 1:
-      analyze_clustering(points, neighbors, clusterings[1])
-    else:
-      analyze_clustering(points, neighbors, clusterings[0])
+  for tc in test_cases:
+    points, neighbors, best, assignments, clusterings = cluster(tc)
+    analyze_clustering(points, neighbors, best)
+    plt.show()
   #cluster(IRIS_DATA)
 
 if __name__ == "__main__":
